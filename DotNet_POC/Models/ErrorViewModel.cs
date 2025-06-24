69ca9f57ac9dfa60ff43904ceb7206bf08b3b8b7
vinay <vinay@PREDACER>
2025-06-24 21:00:57 +0530

    Add project files.

---
+++ DotNet_POC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNet_POC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
